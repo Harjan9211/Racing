0dedfee4b67b3e4ebab760b20f11ecad8d194b2a
Admin <Admin@DESKTOP-17CKR5L>
2018-12-17 12:21:20 +1300

    Add project files.

---
+++ Racing/Models/Bob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racing.Models
+{
+   public class Bob:Punter
+    {
+        //Initialize the Bob class paramater
+        public Bob()
+        {
+            ClassName = "Bob";
+            Money = 45;
+            Type = CommonClass.Punter_Type.Bob;
+        }
+        //Reset the money to initial value
+        public override void Reset()
+        {
+            Money = 45;
+        }
+        //Retrieve money in hand
+        public int GetMoney()
+        {
+            return Money;
+        }
+    }
+}
---
+++ Racing/Models/Joe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racing.Models
+{
+   public class Joe:Punter
+    {
+        // Initialize the Joe class
+        public Joe()
+        {
+            ClassName = "Joe";
+            Money = 40;
+            Type = CommonClass.Punter_Type.Joe;
+        }
+        //Reset the money and initialize to original value
+        public override void Reset()
+        {
+            Money = 40;
+        }
+        //Retrive the money in hand
+        public int GetMoney()
+        {
+            return Money;
+        }
+    }
+}
